916295bd18ee1d4d85e6082aade1e248acf48c37
judyliou <a4142528@gmail.com>
2019-10-07 20:33:56 -0400

    Change schema and add comment

--- a1/db_table.sql
+++ a1/db_table.sql
@@ -5,11 +5,17 @@ CREATE TABLE `users` (
   UNIQUE KEY `userID_UNIQUE` (`userID`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
+
+-- key0: filename for original photo
+-- key1: filename for text-detected photo
+-- key2: filename for text-detected thumbnail
 CREATE TABLE `photos` (
   `userID` varchar(20) NOT NULL,
-  `key0` varchar(100) DEFAULT NULL,
-  `key1` varchar(100) DEFAULT NULL,
+  `key0` varchar(100) NOT NULL,
+  `key1` varchar(100) NOT NULL,
   `key2` varchar(100) NOT NULL,
-  PRIMARY KEY (`key2`)
+  PRIMARY KEY (`key0`),
+  UNIQUE KEY `key0_UNIQUE` (`key0`),
+  UNIQUE KEY `key1_UNIQUE` (`key1`),
+  UNIQUE KEY `key2_UNIQUE` (`key2`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-
