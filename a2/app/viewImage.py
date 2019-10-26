1b79c3a97e20ac0a5d927e66a4e383bd8ebd3890
judyliou <a4142528@gmail.com>
2019-10-26 13:38:02 -0400

    Add viewWorker and copy files from A1

---
+++ a2/app/viewImage.py
@@ -0,0 +1,18 @@
+from app import webapp
+from flask import render_template, redirect, url_for
+
+from app.utils import get_db
+
+
+@webapp.route("/viewImage/<key>", methods=['GET'])
+def viewImage(key):
+    cnx = get_db()
+    cursor = cnx.cursor()
+
+    query = ''' select key0, key1 from photos where key2 = %s '''
+    cursor.execute(query, (key,))
+    keys = cursor.fetchone()
+    url0 = url_for('static', filename='uploads/'+keys[0])
+    url1 = url_for('static', filename='uploads/'+keys[1])
+
+    return render_template("/twoImages.html", key0=url0, key1=url1)
--- a2/app/worker.py
+++ a2/app/worker.py
@@ -1,7 +1,65 @@
-from flask import render_template
+from flask import render_template, url_for, request, redirect
 from app import webapp
+import boto3
+from datetime import datetime, timedelta
+from operator import itemgetter
 
-@webapp.route("/viewWorker/<index>", methods=['GET'])
-def viewWorker(index):
-    print(index)
-    return render_template("workerInfo.html", worker=index)
+@webapp.route("/workers", method=['GET'])
+# Display list of all workers
+def workerList():
+    ec2 = boto3.resource('ec2')
+    instances = ec2.instances.all()
+    return render_template("workerList.html", title='Workers', instances=instances)
+
+
+@webapp.route("/viewWorker/<id>", methods=['GET'])
+# Display details (CPU utilization and HTTP requests received) of a certain worker
+def viewWorker(id):
+    ec2 = boto3.resource('ec2')
+    instance = ec2.instnace(id)
+    client = boto3.client('cloudwatch')
+
+    # CPU utilization
+    cpu = client.get_metric_statistics( 
+        Period=60,
+        StartTime = datetime.utcnow() - timedelta(seconds=60*30),
+        EndTime = datetime.utcnow(),
+        MetricName = 'CPUUtilization',
+        NameSpace = 'AWS/EC2',
+        Statistics = ['Sum'],
+        Dimensions=[{'Name': 'InstanceId', 'Value': id}]
+    )
+
+    cpu_stats = []
+    for point in cpu['Datapoints']:
+        hour = point['Timestamp'].hour
+        minute = point['Timestamp'].minute
+        time = hour + minute/60
+        cpu_stats.append(time, point['Sum'])
+    cpu_stats = sorted(cpu_stats, key=itemgetter(0))
+
+
+    # HTTP request
+    packet_in = client.get_metric_statistics(
+        Period=60,
+        StartTime = datetime.utcnow() - timedelta(seconds=60*30),
+        EndTime = datetime.utcnow(),
+        MetricName = 'HTTPRequest',
+        NameSpace = 'MyNameSpace',
+        Statistics = ['Sum'],
+        Dimensions=[{'Name': 'InstanceId', 'Value': id}]
+    )
+   
+    HTTP_stats = []
+    for point in HTTP_stats['Datapoints']:
+        hour = point['Timestamp'].hour
+        minute = point['Timestamp'].minute
+        time = hour + minute/60
+        HTTP_stats.append([time, point['Sum']])
+    HTTP_stats = sorted(HTTP_stats, key=itemgetter(0))
+
+    return render_template("workerInfo.html", 
+                            worker=id,   
+                            instance=instance,
+                            cpu_stats=cpu_stats,
+                            HTTP_stats=HTTP_stats)
