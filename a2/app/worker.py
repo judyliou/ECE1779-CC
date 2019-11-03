bd66397623a05811a671af5aa9fe3e5233124e15
judyliou <a4142528@gmail.com>
2019-11-03 14:23:20 -0500

    Update chart x-axis to time format

--- a2/app/worker.py
+++ a2/app/worker.py
@@ -36,9 +36,10 @@ def viewWorker(id):
 
     cpu_stats = []
     for point in cpu['Datapoints']:
-        hour = point['Timestamp'].hour
-        minute = point['Timestamp'].minute
-        time = hour + minute/60
+        # hour = point['Timestamp'].hour
+        # minute = point['Timestamp'].minute
+        # time = hour + minute/60
+        time = point['Timestamp'].timestamp( )*1000
         cpu_stats.append([time, point['Average']])
     cpu_stats = sorted(cpu_stats, key=itemgetter(0))
 
@@ -56,9 +57,10 @@ def viewWorker(id):
    
     http_stats = []
     for point in http_in['Datapoints']:
-        hour = point['Timestamp'].hour
-        minute = point['Timestamp'].minute
-        time = hour + minute/60
+        # hour = point['Timestamp'].hour
+        # minute = point['Timestamp'].minute
+        # time = hour + minute/60
+        time = point['Timestamp'].timestamp( )*1000
         http_stats.append([time, point['Sum']])
     http_stats = sorted(http_stats, key=itemgetter(0))
     # print('cpu:', cpu_stats)
