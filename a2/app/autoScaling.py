e698afb1a2fdd32be6c68664575895ba2b8fd585
judyliou <a4142528@gmail.com>
2019-11-04 08:20:02 -0500

    Update worker chart

--- a2/app/autoScaling.py
+++ a2/app/autoScaling.py
@@ -1,5 +1,7 @@
 import boto3
 from datetime import datetime, timedelta
+import threading
+
 from app.utils import *
 from app import awsUtils
 
@@ -16,26 +18,34 @@ flag = 0
 
 # connect to CloudWatch
 client = boto3.client('cloudwatch')
-cpu = client.get_metric_statistics( 
-        Period=60,
-        StartTime = datetime.utcnow() - timedelta(seconds=60*2),
-        EndTime = datetime.utcnow(),
-        MetricName = 'CPUUtilization',
-        Namespace = 'AWS/EC2',
-        Statistics = ['Average']
-    )
-cpu_record = []
-for point in cpu['Datapoints']:
-    cpu_record.append(point['Average'])
+
 
 
 # Alter worker pool
 awsSuite = awsUtils.AWSSuite()
 
-if sum(cpu_record)/len(cpu_record) > threshold:
-    awsSuite.growWorker(ratio)
-    flag = 1
-    # when to set flag back to 0????
-elif sum(cpu_record)/len(cpu_record) > threshold:
-    awsSuite.shrinkWorker(ratio)
-    flag = 1
+def check_status(threshold, ratio, flag):
+    threading.Timer(120, check_status, (threshold, ratio)).start()
+    
+    if flag == 0:
+        # get CPU data
+        cpu = client.get_metric_statistics( 
+            Period=60,
+            StartTime = datetime.utcnow() - timedelta(seconds=60*2),
+            EndTime = datetime.utcnow(),
+            MetricName = 'CPUUtilization',
+            Namespace = 'AWS/EC2',
+            Statistics = ['Average']
+        )
+        cpu_record = []
+        for point in cpu['Datapoints']:
+            cpu_record.append(point['Average'])
+
+        # check wether over the threshold
+        if sum(cpu_record)/len(cpu_record) > threshold:
+            awsSuite.growWorker(ratio)
+            flag = 1
+            # when to set flag back to 0????
+        elif sum(cpu_record)/len(cpu_record) > threshold:
+            awsSuite.shrinkWorker(ratio)
+            flag = 1
--- a2/app/config/awsConfig.py
+++ a2/app/config/awsConfig.py
@@ -16,5 +16,5 @@
 imageId = "ami-0504cab097c420742"
 arn = "arn:aws:elasticloadbalancing:us-east-1:735141600372:targetgroup/a1group/bb2f4c7ebd44cf9b"
 workerTag = {"key": "type", "value": "worker"}
-keypair = "ece1779"
-securityGroup = "launch-wizard-1"
+keypair = "ece1779_a1"
+securityGroup = "launch-wizard-3"
