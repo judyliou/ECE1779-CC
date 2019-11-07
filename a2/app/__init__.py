beedd12abace35c47ebf4bc9d3b971c40779b388
HarshithaRan <56305626+HarshithaRan@users.noreply.github.com>
2019-11-06 23:32:48 -0500

    Update __init__.py

--- a2/app/__init__.py
+++ a2/app/__init__.py
@@ -8,5 +8,12 @@ webapp.config["SECRET_KEY"] = SECRET_KEY
 webapp.config['PERMANENT_SESSION_LIFETIME'] = timedelta(hours=24)
 webapp.config['MAX_CONTENT_LENGTH'] = 50 * 1024 * 1024
 
+from app import login
+from app import upload
+from app import viewImage
+from app.api import blueprint as api
+
+webapp.register_blueprint(api, url_prefix='/api')
+
 from app import worker
 from app import manager
